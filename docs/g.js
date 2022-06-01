444414d98b1bb4ac80b428f323faa49a8768cfa2
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-06-01 04:31:01 +0000

    halo halo haa lo

--- docs/g.js
+++ docs/g.js
@@ -766,14 +766,14 @@ async function sw(_r,_a,_f,_t,_m){
 	alert("sw.completed")
 }
 
-async function de(a,m,f,i,t){
+async function de(a,m,f,i){
 	if(!isFinite(_a)){alert('malformed input amount!');return}
 	_a = (_a*10**DEC[_f])
 	_m = (_m*10**DEC[_t])
 	if(B<_a*10**DEC[_f]){alert("Not Enough Balance!\nhave="+B/10**DEC[_f]+"\nwant="+_a/10**DEC[_f]);return}
 	D = new ethers.Contract(RUTR[0],abix,signer)
-	let _tr = await D.swap(BigInt(a),slip(m),{from:f,into:i,to:t})
-	alert("de=>"+a+m+f+i+t)
+	let _tr = await D.swap(BigInt(a),slip(m),{from:f,into:i,to:window.ethereum.selectedAddress})
+	alert("de=>"+a+m+f+i)
 	await _tr.wait()
 	alert("de.completed")
 }
