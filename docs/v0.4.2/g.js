ae14ab61047fc5ba7af76c3a83f1b232b42616ca
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-06-22 02:23:56 +0000

    @2

--- docs/v0.4.2/g.js
+++ docs/v0.4.2/g.js
@@ -700,7 +700,6 @@ async function pairn() {
 	console.log("pair:n=",n,tokes[0][1],tokes[1][1])
 
 	$("ao_0").value = "0.00"
-	await Promise.all([	getao($("ain").value), getAppr(tokes[1][0]) ])
 
 	$("img_in").src=tokes[1][3]
 	$("img_out").src=tokes[0][3]
@@ -708,6 +707,7 @@ async function pairn() {
 	$("swap_in").innerHTML="Sell "+tokes[1][1]
 	$("swap_out").innerHTML="Buy "+tokes[0][1]
 
+	await Promise.all([	getao($("ain").value), chkAppr(tokes[1][0]) ])
 
 }
 
